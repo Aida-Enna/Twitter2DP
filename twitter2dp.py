88ec35811c6eee212c114d8d90f7a3156fb64047
Aida Enna <Aida-Enna@users.noreply.github.com>
2024-02-29 04:57:30 -0500

    Moved from discord bot to webhook

--- twitter2dp.py
+++ twitter2dp.py
@@ -2,18 +2,17 @@
 # People must change. Otherwise, even if humanity expands into space, it will only create new
 # conflicts, and that would be a very sad thing. - Aeolia Schenberg, 2091 A.D.
 
-
 import ast
-from colorama import Fore, Back, Style
-import discord
+from colorama import Fore, Back, Style, init
 import json
+import requests
 import os
 import time
 from dotenv import load_dotenv
 from twikit import Client
 
 load_dotenv()
-bot = discord.Bot()
+init(autoreset=True)
 
 YOUR_TWITTER_USERNAME = os.getenv('YOUR_TWITTER_USERNAME')
 YOUR_TWITTER_EMAIL = os.getenv('YOUR_TWITTER_EMAIL')
@@ -28,15 +27,28 @@ client.login(
     auth_info_2=YOUR_TWITTER_EMAIL,
     password=YOUR_TWITTER_PASSWORD
 )
-		
-@bot.event
-async def on_ready():
-	print(f"{bot.user} is ready and online, checking every {CHECK_TIMER_IN_SECONDS} seconds!")
-	while True:
-		await get_latest_tweets()
-		time.sleep(int(CHECK_TIMER_IN_SECONDS))
 
-async def get_latest_tweets():
+def check_users_json():
+	try:
+		raw_json = open('users.json')
+		twitter_users = json.load(raw_json)
+	except:
+		print(Fore.RED + f"ERROR: Something went wrong with loading the users.json file. Please check that it is a valid JSON and try again.")
+		exit()
+
+	for twitter_user in twitter_users:
+		#print(twitter_user)
+		if 'account_to_check' not in twitter_user:
+			print(Fore.RED + 'ERROR: One of the JSON entries is missing an account_to_check entry! Please check the users.json file and relaunch.')
+			exit()
+		if 'posting_text' not in twitter_user:
+			print(Fore.RED + f"ERROR: The JSON entry for {twitter_user['account_to_check']} is missing a posting_text entry! Please check the users.json file and relaunch.")
+			exit()
+		if 'webhook_url' not in twitter_user:
+			print(Fore.RED + f"ERROR: The JSON entry for {twitter_user['account_to_check']} is missing a webhook_url entry! Please check the users.json file and relaunch.")
+			exit()
+
+def get_latest_tweets():
 	raw_json = open('users.json')
 	twitter_users = json.load(raw_json)
 
@@ -53,12 +65,16 @@ async def get_latest_tweets():
 			continue
 		
 		#omg a hit tweet, let's post it
-		channel = bot.get_channel(int(twitter_user['posting_channel']))
+		#channel = bot.get_channel(int(twitter_user['posting_channel']))
 		#https://twitter.com/Username/status/TweetID
 		tweetlink = f'https://twitter.com/{twitter_user["account_to_check"]}/status/{user_tweets[0].id}'
-		print(f"New tweet found from {twitter_user['account_to_check']} (ID: {user_tweets[0].id}), sending it on it's way to channel {twitter_user['posting_channel']}!")
+		
+		print(f"New tweet found from {twitter_user['account_to_check']} (ID: {user_tweets[0].id}), sending it to the webhook!")
+		
 		#replace the keyword <tweetlink> with the actual twitter link.
-		await channel.send(twitter_user['posting_text'].replace("<tweetlink>", tweetlink))
+		payload = {'content': twitter_user['posting_text'].replace("<tweetlink>", tweetlink)}
+		response = requests.post(twitter_user['webhook_url'], json=payload)
+		
 		#save the previous ID so we don't keep posting
 		twitter_user['previous_tweet_id'] = user_tweets[0].id
 	
@@ -73,6 +89,7 @@ if __name__ == '__main__':
 	print(Fore.CYAN + '   ██║   ██║███╗██║██║   ██║      ██║   ██╔══╝  ██╔══██╗██╔═══╝ ██║  ██║██╔═══╝ ')
 	print(Fore.CYAN + '   ██║   ╚███╔███╔╝██║   ██║      ██║   ███████╗██║  ██║███████╗██████╔╝██║     ')
 	print(Fore.CYAN + '   ╚═╝    ╚══╝╚══╝ ╚═╝   ╚═╝      ╚═╝   ╚══════╝╚═╝  ╚═╝╚══════╝╚═════╝ ╚═╝     ')
-	print(Style.RESET_ALL)
-	bot.run(os.getenv('DISCORD_BOT_TOKEN'))
+	print('')
+	check_users_json()
+	get_latest_tweets()
 	
